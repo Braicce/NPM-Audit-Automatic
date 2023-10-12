27ec6207154ef48eb9252ef18f7bfbc819767831
Bruno Bricce <62706067+Braicce@users.noreply.github.com>
2023-10-12 15:09:55 -0300

    Update npmaudit.sh

--- npmaudit.sh
+++ npmaudit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Defina os servidores que deseja se conectar em uma matriz
+# Definir os servidores que deseja se conectar
 servers=(
    IPSERVER1
    IPSERVER2
@@ -16,8 +16,8 @@ do
 
 echo "Conectando em $server..."
 
-# Conecte-se ao servidor via SSH e execute a busca pelo arquivo package.json
+# Comando que irá se conectar ao servidor via SSH e executar a busca pelo arquivo package.json / Altere a pasta NODE_FOLDER
 
-    ssh -i $ssh_key $user@$server 'echo "Procurando a pasta"; find /opt/wwwroot/node_folder -name "package.json" 2>/dev/null | while read pkg; do cd "$(dirname "$pkg")"; echo "executando npm  $pkg"; npm audit >> /tmp/resultados.txt; cat /tmp/resultados.txt; done; echo "apagando arquivo resultados"; cd /tmp/ && rm -f resultados.txt';
+    ssh -i $ssh_key $user@$server 'echo "Procurando a pasta"; find /opt/wwwroot/NODE_FOLDER -name "package.json" 2>/dev/null | while read pkg; do cd "$(dirname "$pkg")"; echo "executando npm  $pkg"; npm audit >> /tmp/resultados.txt; cat /tmp/resultados.txt; done; echo "apagando arquivo resultados"; cd /tmp/ && rm -f resultados.txt';
 	
 done
